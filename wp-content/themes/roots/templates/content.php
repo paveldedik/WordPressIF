d7afe05d3b7e00740a061391aa096e38a073327e
Pavel Dedik <dedikx@gmail.com>
2013-09-11 13:04:05 +0200

    Show thumbnail next to post.

--- wp-content/themes/roots/templates/content.php
+++ wp-content/themes/roots/templates/content.php
@@ -1,5 +1,10 @@
 <article <?php post_class(); ?>>
   <header>
+    <?php if (has_post_thumbnail()) { ?>
+    <a href="<?php the_permalink(); ?>" class="thumbnail" title="<?php the_title_attribute(); ?>" >
+      <?php the_post_thumbnail('thumbnail'); ?>
+    </a>
+    <?php } ?>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php get_template_part('templates/entry-meta'); ?>
   </header>
