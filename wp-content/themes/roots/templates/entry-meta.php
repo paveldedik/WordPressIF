2de0d27fb5a18927ea9469c54a902b2d6c41fc2f
Pavel Dedik <dedikx@gmail.com>
2013-09-11 17:26:09 +0200

    Added czech localization.

--- wp-content/themes/roots/templates/entry-meta.php
+++ wp-content/themes/roots/templates/entry-meta.php
@@ -1,7 +1,7 @@
 <p class="author">
     <time class="updated" datetime="<?php echo get_the_time('c'); ?>" pubdate>
         <?php echo get_the_date(); ?>
-    </time> | <?php echo __('By', 'roots'); ?>
+    </time> &sdot; <?php echo __('Written by', 'roots'); ?>
     <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn">
         <?php echo get_the_author(); ?>
     </a>
