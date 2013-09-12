d6702d1e83c9ef13a899a9bf1bd530ece2b6e228
Pavel Dedik <dedikx@gmail.com>
2013-09-12 10:56:50 +0200

    Fixed search form and page navigator.

---
+++ wp-content/themes/roots/searchform.php
@@ -0,0 +1,7 @@
+<form role="search" method="get" id="searchform" class="form-search" action="<?php echo home_url('/'); ?>">
+  <label class="hide" for="s"><?php _e('Search for:', 'roots'); ?></label>
+  <div class="input-append">
+    <input type="text" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" id="s" class="search-query" placeholder="<?php _e('Search', 'roots'); ?> <?php bloginfo('name'); ?>">
+    <input type="submit" id="searchsubmit" value="<?php _e('Search', 'roots'); ?>" class="btn btn-primary">
+  </div>
+</form>
